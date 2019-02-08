a7a24b30d566306cad4221401784db5d6a9fa1c3
João Costa <jdpc557@gmail.com>
2019-02-08 23:39:17 +0000

    Add public minimize method

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 //! A Nelder-Mead simplex optimizer
 
 pub mod params;
-pub mod simplex;
+mod simplex;
 mod algebra;
+
+use crate::simplex::*;
+use crate::params::*;
+
+pub fn minimize(
+    f: &Function,
+    initial_point: Point,
+    initial_simplex_size: f64,
+    params: Params,
+    max_iter: u32,
+) -> (Point, f64) {
+    let initial_simplex = new_simplex(f, initial_point, initial_simplex_size);
+    crate::simplex::minimize(f, initial_simplex, params, max_iter)
+}
